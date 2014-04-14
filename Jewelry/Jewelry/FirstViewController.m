01648b9fea0a62d46366a8cee728c70b19829681
zzzfly121 <zzzfly121@gmail.com>
2014-04-14 23:31:55 +0800

    add project files

---
+++ Jewelry/Jewelry/FirstViewController.m
@@ -0,0 +1,29 @@
+//
+//  FirstViewController.m
+//  Jewelry
+//
+//  Created by lv on 14-4-14.
+//  Copyright (c) 2014年 FE. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
